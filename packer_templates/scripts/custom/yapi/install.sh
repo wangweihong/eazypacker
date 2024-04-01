bac7479fb43a17862b5cb68bb939cbdd5e0c9ae6
wangweihong <wangweihong2018@qq.com>
2024-04-01 12:12:13 +0000

    fix(yapi): specfic mongo version

--- packer_templates/scripts/custom/yapi/install.sh
+++ packer_templates/scripts/custom/yapi/install.sh
@@ -2,6 +2,6 @@
 set -e
 set -x
 docker pull registry.cn-hangzhou.aliyuncs.com/anoy/yapi
-docker run -d --name mongo-yapi -p 27017:27017 -v /root/yapi:/data/db mongo
+docker run -d --name mongo-yapi -p 27017:27017 -v /root/yapi:/data/db mongo:5.0
 docker run -it --rm --link mongo-yapi:mongo --entrypoint npm --workdir /api/vendors registry.cn-hangzhou.aliyuncs.com/anoy/yapi run install-server
 docker run -d --name yapi --link mongo-yapi:mongo --workdir /api/vendors -p 10001:3000/tcp registry.cn-hangzhou.aliyuncs.com/anoy/yapi server/app.js
