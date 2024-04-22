9da02577ac78595fb99bf69f219d92fdc95a4609
wangweihong <wangweihong2018@qq.com>
2024-04-22 14:11:49 +0800

    feat(elk): support es basic security install

--- packer_templates/scripts/custom/elk/install.sh
+++ packer_templates/scripts/custom/elk/install.sh
@@ -7,29 +7,43 @@ ESNAME="es01"
 ESNET="elk"
 SUBNET="172.22.0.0/16"
 ELK_IP="172.22.0.100"
-RootDIR=/es
-mkdir -p $RootDIR
-chmod +777 $RootDIR
+ROOT_DIR=/es
+CA_PASSWORD=elastic
 
+mkdir -p $ROOT_DIR
+chmod +777 $ROOT_DIR
 
 # 拷贝配置出来
-if [ ! -e $RootDIR/config ];then 
-    sudo -s docker run  -d --rm  --name esinit  docker.elastic.co/elasticsearch/elasticsearch:$ESVERSION
+if [ ! -e $ROOT_DIR/config ]; then
+    sudo -s docker run -d --rm --name esinit docker.elastic.co/elasticsearch/elasticsearch:$ESVERSION
 
-    mkdir -p $RootDIR/config
-    sudo -s docker cp esinit:/usr/share/elasticsearch/config $RootDIR/
-    sudo -s docker stop esinit 
+    # sudo -s docker exec esinit elasticsearch-certutil ca --out /usr/share/elasticsearch/config/elastic-stack-ca.p12 --pass $CA_PASSWORD --days 3650
+    # # generate certificates
+    # sudo -s docker exec esinit elasticsearch-certutil cert \
+    #         --ca /usr/share/elasticsearch/config/elastic-stack-ca.p12 --ca-pass $CA_PASSWORD \
+    #         --out /usr/share/elasticsearch/config/elastic-certificates.p12 --pass elastic
 
+    mkdir -p $ROOT_DIR/config
+    sudo -s docker cp esinit:/usr/share/elasticsearch/config $ROOT_DIR/
+
+    # chmod 666 $ROOT_DIR/config/elastic-stack-ca.p12
+    # chmod 666 $ROOT_DIR/config/elastic-certificates.p12
+
+    # mkdir -p $ROOT_DIR/pki
+    # sudo -s docker exec esinit mkdir -p /usr/share/elasticsearch/pki
+    # generate ca
+
+    sudo -s docker stop esinit
 fi
 
-mkdir -p $RootDIR/data
-mkdir -p $RootDIR/log 
-mkdir -p $RootDIR/plugin
+mkdir -p $ROOT_DIR/data
+mkdir -p $ROOT_DIR/log
+mkdir -p $ROOT_DIR/plugin
 
-chmod +777 $RootDIR/config 
-chmod +777 $RootDIR/data
-chmod +777 $RootDIR/log 
-chmod +777 $RootDIR/plugin
+chmod +777 $ROOT_DIR/config
+chmod +777 $ROOT_DIR/data
+chmod +777 $ROOT_DIR/log
+chmod +777 $ROOT_DIR/plugin
 
 # harbor默认使用172.18.0.0/16的网段, 避免可能的冲突
 docker network create $ESNET --subnet=$SUBNET || true
@@ -37,21 +51,47 @@ docker network create $ESNET --subnet=$SUBNET || true
 # -e "discovery.type=single-node" -m 1G 设置java的堆内存
 # 如果服务器内存不足, 会导致elasticsearch无法启动： ERROR: Elasticsearch exited unexpectedly, with exit code 137
 sudo -s docker run --restart=always -d --name $ESNAME \
-    --net $ESNET --ip $ELK_IP -p 9200:9200 -p 9300:9300  \
+    --net $ESNET --ip $ELK_IP -p 9200:9200 -p 9300:9300 \
     --privileged \
-    -v $RootDIR/data:/usr/share/elasticsearch/data \
-    -v $RootDIR/log:/usr/share/elasticsearch/logs \
-    -v $RootDIR/config:/usr/share/elasticsearch/config \
-    -v $RootDIR/plugin:/usr/share/elasticsearch/plugins \
+    -v $ROOT_DIR/data:/usr/share/elasticsearch/data \
+    -v $ROOT_DIR/log:/usr/share/elasticsearch/logs \
+    -v $ROOT_DIR/config:/usr/share/elasticsearch/config \
+    -v $ROOT_DIR/plugin:/usr/share/elasticsearch/plugins \
     -e "discovery.type=single-node" -m 1G \
     docker.elastic.co/elasticsearch/elasticsearch:$ESVERSION
 
-if ! [ -e $RootDIR/elasticsearch-analysis-ik-$ESVERSION.zip ];then
-  curl -L  https://github.com/infinilabs/analysis-ik/releases/download/v$ESVERSION/elasticsearch-analysis-ik-$ESVERSION.zip -o $RootDIR/elasticsearch-analysis-ik-$ESVERSION.zip
-  if ! [ -x "$(command -v zip)" ]; then
-      apt install -y zip
-  fi
+cat <<EOF >>$ROOT_DIR/config/elasticsearch.yml
+xpack.security.transport.ssl.enabled: true
+xpack.security.transport.ssl.verification_mode: certificate
+xpack.security.transport.ssl.client_authentication: required
+xpack.security.transport.ssl.truststore.path: elastic-certificates.p12
+xpack.security.transport.ssl.keystore.path: elastic-certificates.p12
+EOF
+
+if [ "${ES_TLS+isset}" = "isset" ] && [ "${ES_TLS}" = "true" ]; then
+    # https://www.elastic.co/guide/en/elasticsearch/reference/8.13/security-basic-setup.html#encrypt-internode-communication
+    echo "config basid security for elasticsearch..."
+    sudo -s docker exec $ESNAME elasticsearch-certutil ca --out /usr/share/elasticsearch/config/elastic-stack-ca.p12 --pass $CA_PASSWORD --days 3650
+    # generate certificates
+    sudo -s docker exec $ESNAME elasticsearch-certutil cert \
+        --ca /usr/share/elasticsearch/config/elastic-stack-ca.p12 --ca-pass $CA_PASSWORD \
+        --out /usr/share/elasticsearch/config/elastic-certificates.p12 --pass $CA_PASSWORD
+
+    # 证书密码到keystore
+    # 不能直接通过配置文件来设置。
+    echo $CA_PASSWORD | sudo -s docker exec -i es01 elasticsearch-keystore add xpack.security.transport.ssl.keystore.secure_password -s
+    echo $CA_PASSWORD | sudo -s docker exec -i es01 elasticsearch-keystore add xpack.security.transport.ssl.truststore.secure_password -s
+
+    chmod 666 $ROOT_DIR/config/elastic-stack-ca.p12
+    chmod 666 $ROOT_DIR/config/elastic-certificates.p12
 fi
-unzip $RootDIR/elasticsearch-analysis-ik-$ESVERSION.zip -d $RootDIR/plugin/elasticsearch-analysis-ik-$ESVERSION
+
+if ! [ -e $ROOT_DIR/elasticsearch-analysis-ik-$ESVERSION.zip ]; then
+    curl -L https://github.com/infinilabs/analysis-ik/releases/download/v$ESVERSION/elasticsearch-analysis-ik-$ESVERSION.zip -o $ROOT_DIR/elasticsearch-analysis-ik-$ESVERSION.zip
+    if ! [ -x "$(command -v zip)" ]; then
+        apt install -y zip
+    fi
+fi
+unzip $ROOT_DIR/elasticsearch-analysis-ik-$ESVERSION.zip -d $ROOT_DIR/plugin/elasticsearch-analysis-ik-$ESVERSION
 
 docker restart $ESNAME
